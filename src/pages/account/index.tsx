e51f63e5336209fcfc9fa5f6f2c78eb516960e5f
IvanZen <247460386@qq.com>
2021-06-22 19:03:02 +0800

    Add: page 'history'

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ExclamationCircleOutlined, UserOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Modal, Table, Tag, Space, Switch, Select, Avatar, Image, Tooltip, Button } from 'antd';
+import { Modal, Table, Tag, Space, Switch, Select, Avatar, Image, Tooltip, Button, Card } from 'antd';
 import { useRequest } from 'umi';
 import request from 'umi-request';
 
@@ -67,86 +67,88 @@ function showPromiseConfirm(value: string) {
 const index: React.FC = () => {
   return (
     <PageContainer>
-      <Table dataSource={data}>
-        <Column
-          title="Avatar"
-          dataIndex="avator"
-          key="avator"
-          render={(avator) => (
-            <Avatar
-              shape="square"
-              size={64}
-              //   src={<Image src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-              src={<Image src={avator} />}
-            />
-          )}
-        />
-
-        <Column title="User name" dataIndex="userName" key="userName" />
-
-        <Column
-          title="Address"
-          dataIndex="address"
-          key="address"
-          render={(address) => (
-            <Tooltip placement="topLeft" title={address}>
-              <span>{`${address.slice(0, 6)}...${address.slice(-4)}`}</span>
-            </Tooltip>
-          )}
-        />
-
-        <Column title="Email" dataIndex="Email" key="Email" />
-
-        <Column title="Creation" dataIndex="creation" key="creation" />
-
-        <Column
-          title="Identity"
-          dataIndex="Identity"
-          key="Identity"
-          render={() => (
-            <Select
-              defaultValue="User"
-              style={{ width: 120 }}
-              onChange={(value) => {
-                console.log(value);
-              }}
-            >
-              <Option value="User">User</Option>
-              <Option value="Administor">Administor</Option>
-            </Select>
-          )}
-        />
-
-        <Column
-          title="Hide Creation"
-          key="hide"
-          render={() => (
-            <Switch
-              loading={false}
-              defaultChecked={false}
-              checkedChildren="hiden"
-              unCheckedChildren="show"
-              onChange={(checked: boolean, event: Event) => {
-                // console.log("event: ", event)
-              }}
-            />
-          )}
-        />
-
-        <Column
-          title="Disable"
-          key="disable"
-          render={() => (
-            <Switch
-              loading={false}
-              defaultChecked={false}
-              checkedChildren="disable"
-              unCheckedChildren="active"
-              onChange={(checked: boolean) => {}}
-            />
-          )}
-        />
-      </Table>
+      <Card bordered={false}>
+        <Table dataSource={data}>
+          <Column
+            title="Avatar"
+            dataIndex="avator"
+            key="avator"
+            render={(avator) => (
+              <Avatar
+                shape="square"
+                size={64}
+                //   src={<Image src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                src={<Image src={avator} />}
+              />
+            )}
+          />
+        
+          <Column title="User name" dataIndex="userName" key="userName" />
+        
+          <Column
+            title="Address"
+            dataIndex="address"
+            key="address"
+            render={(address) => (
+              <Tooltip placement="topLeft" title={address}>
+                <span>{`${address.slice(0, 6)}...${address.slice(-4)}`}</span>
+              </Tooltip>
+            )}
+          />
+        
+          <Column title="Email" dataIndex="Email" key="Email" />
+        
+          <Column title="Creation" dataIndex="creation" key="creation" />
+        
+          <Column
+            title="Identity"
+            dataIndex="Identity"
+            key="Identity"
+            render={() => (
+              <Select
+                defaultValue="User"
+                style={{ width: 120 }}
+                onChange={(value) => {
+                  console.log(value);
+                }}
+              >
+                <Option value="User">User</Option>
+                <Option value="Administor">Administor</Option>
+              </Select>
+            )}
+          />
+        
+          <Column
+            title="Hide Creation"
+            key="hide"
+            render={() => (
+              <Switch
+                loading={false}
+                defaultChecked={false}
+                checkedChildren="hiden"
+                unCheckedChildren="show"
+                onChange={(checked: boolean, event: Event) => {
+                  // console.log("event: ", event)
+                }}
+              />
+            )}
+          />
+        
+          <Column
+            title="Disable"
+            key="disable"
+            render={() => (
+              <Switch
+                loading={false}
+                defaultChecked={false}
+                checkedChildren="disable"
+                unCheckedChildren="active"
+                onChange={(checked: boolean) => {}}
+              />
+            )}
+          />
+        </Table>
+      </Card>
     </PageContainer>
   );
 };
--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,21 +1,132 @@
-import React from 'react'
+import React from 'react';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, List, Image, Tag, Tabs, Input, Space, message, Modal, Button, Tooltip } from 'antd';
+import {
+  Card,
+  List,
+  Image,
+  Tag,
+  Tabs,
+  Input,
+  Space,
+  message,
+  Modal,
+  Button,
+  Tooltip,
+  Table,
+} from 'antd';
 import { useRequest } from 'umi';
 import request from 'umi-request';
-
 
 const { TabPane } = Tabs;
 const { Search } = Input;
 const { confirm } = Modal;
+const { Column, ColumnGroup } = Table;
 
-const index:React.FC = () => {
-    return (
-        <PageContainer>
-            <h1>History Page</h1>
-        </PageContainer>
-    )
-}
+const data = [
+  {
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
-export default index
+const index: React.FC = () => {
+  return (
+    <PageContainer>
+      <Card bordered={false}>
+        <h1>History Page</h1>
+        <Table dataSource={data}>
+          <ColumnGroup title="Name">
+            <Column
+              title="First Name"
+              dataIndex="firstName"
+              key="firstName"
+              filters={[
+                {
+                  text: 'Joe',
+                  value: 'Joe',
+                },
+                {
+                  text: 'Jim',
+                  value: 'Jim',
+                },
+                {
+                  text: 'Submenu',
+                  value: 'Submenu',
+                  children: [
+                    {
+                      text: 'Green',
+                      value: 'Green',
+                    },
+                    {
+                      text: 'Black',
+                      value: 'Black',
+                    },
+                  ],
+                },
+              ]}
+              onFilter={(value, record) => record.firstName.indexOf(value) === 0}
+            />
+            <Column title="Last Name" dataIndex="lastName" key="lastName" />
+          </ColumnGroup>
+          <Column
+            title="Age"
+            dataIndex="age"
+            key="age"
+            sorter={(a, b) => {
+              console.log(a, b);
+              return a.age - b.age;
+            }}
+          />
+          <Column title="Address" dataIndex="address" key="address" />
+          <Column
+            title="Tags"
+            dataIndex="tags"
+            key="tags"
+            render={(tags) => (
+              <>
+                {tags.map((tag) => (
+                  <Tag color="blue" key={tag}>
+                    {tag}
+                  </Tag>
+                ))}
+              </>
+            )}
+          />
+          <Column
+            title="Action"
+            key="action"
+            render={(text, record) => (
+              <Space size="middle">
+                <a>Invite {record.lastName}</a>
+                <a>Delete</a>
+              </Space>
+            )}
+          />
+        </Table>
+      </Card>
+    </PageContainer>
+  );
+};
+
+export default index;

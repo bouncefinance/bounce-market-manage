7c12469d767ac7f5452453d74312419492f81a0b
IvanZen <247460386@qq.com>
2021-06-25 10:37:19 +0800

    Edit routes name

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -15,6 +15,9 @@ import {
   Card,
   message,
   Input,
+  Row,
+  Col,
+  Statistic,
 } from 'antd';
 import { useRequest } from 'umi';
 import request from 'umi-request';
@@ -119,163 +122,33 @@ const index: React.FC = () => {
 
   return (
     <PageContainer>
-      <Space direction={'vertical'} style={{ width: '100%' }}>
-        <Search
-          placeholder="input search text"
-          allowClear
-          onSearch={(value) => searchAccount({ current: 1, pageSize: 7 }, value)}
-          style={{ width: '75%' }}
-          size="middle"
-        />
-        <Card bordered={false}>
-          <Table {...accountTableProps}>
-            <Column
-              title="Avatar"
-              dataIndex="imgurl"
-              key="imgurl"
-              width={110}
-              align={'center'}
-              render={(imgurl) => (
-                <Image
-                  src={imgurl}
-                  width={64}
-                  height={64}
-                  style={{ objectFit: 'contain' }}
-                  placeholder={
-                    <Image
-                      preview={false}
-                      src={placeholderImg}
-                      width={64}
-                      height={64}
-                      style={{ background: 'white' }}
-                    />
-                  }
-                />
-              )}
-            />
-
-            <Column
-              title="User name"
-              dataIndex="username"
-              key="username"
-              align={'center'}
-              ellipsis={{ showTitle: false }}
-              render={(username) =>
-                username ? (
-                  <Tooltip placement="topLeft" title={username}>
-                    <span>{username}</span>
-                  </Tooltip>
-                ) : (
-                  '--'
-                )
-              }
-            />
-
-            <Column
-              title="Address"
-              dataIndex="accountaddress"
-              key="accountaddress"
-              width={120}
-              align={'center'}
-              render={(accountaddress) => (
-                <Tooltip placement="topLeft" title={accountaddress}>
-                  <span>{`${accountaddress.slice(0, 6)}...${accountaddress.slice(-4)}`}</span>
-                </Tooltip>
-              )}
-            />
-
-            <Column
-              title="Email"
-              dataIndex="email"
-              key="email"
-              align={'center'}
-              ellipsis={{ showTitle: false }}
-              render={(email) =>
-                email ? (
-                  <Tooltip placement="topLeft" title={email}>
-                    <span>{email}</span>
-                  </Tooltip>
-                ) : (
-                  '--'
-                )
-              }
-            />
-
-            <Column
-              title="Identity"
-              dataIndex="Identity"
-              key="Identity"
-              align={'center'}
-              render={() => (
-                <Select
-                  defaultValue="User"
-                  style={{ width: 124 }}
-                  onChange={(value) => {
-                    console.log(value);
-                  }}
-                >
-                  <Option value="User">User</Option>
-                  <Option value="verifiedUser">Verified User</Option>
-                </Select>
-              )}
-            />
-
-            <Column
-              title="Hide Creation"
-              key="hide"
-              width={110}
-              align={'center'}
-              render={() => (
-                <Switch
-                  loading={false}
-                  defaultChecked={false}
-                  checkedChildren="hiden"
-                  unCheckedChildren="show"
-                  onChange={(checked: boolean, event: Event) => {
-                    // console.log("event: ", event)
-                  }}
-                />
-              )}
-            />
-
-            <Column
-              title="Disable"
-              key="disable"
-              width={100}
-              align={'center'}
-              render={() => (
-                <Switch
-                  loading={false}
-                  defaultChecked={false}
-                  checkedChildren="disable"
-                  unCheckedChildren="active"
-                  onChange={(checked: boolean, event: Event) => {
-                    // console.log("event: ", event)
-                  }}
-                />
-              )}
-            />
-
-            <Column
-              title="Delete"
-              key="delete"
-              width={110}
-              align={'center'}
-              render={(record: IAccountData) => (
-                <Button
-                  danger
-                  key="list-loadmore-delete"
-                  onClick={() => {
-                    handleDeleteAccount(record.id, reloadAccount);
-                  }}
-                >
-                  Delete
-                </Button>
-              )}
-            />
-          </Table>
-        </Card>
-      </Space>
+      <Row gutter={16}>
+        <Col span={4}>
+          <Card>
+            <Statistic groupSeparator="," title="NFTs on sale" value={256568} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card>
+            <Statistic groupSeparator="," title="NFTs generated" value={256568} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card>
+            <Statistic groupSeparator="," title="Total wallets(accounts)" value={112893} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card>
+            <Statistic groupSeparator="," title="volumn (24h)" value={112893} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card>
+            <Statistic groupSeparator="," title="volumn" value={112893} />
+          </Card>
+        </Col>
+      </Row>
     </PageContainer>
   );
 };

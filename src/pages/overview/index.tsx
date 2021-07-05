13e9fdba08554352491502bf8a16b0f6427d0f9c
IvanZen <247460386@qq.com>
2021-07-05 20:17:35 +0800

    fix: add minwidth in Row in page 'overview'

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import {
-  Card,
-  Row,
-  Col,
-  Statistic,
-} from 'antd';
+import { Card, Row, Col, Statistic } from 'antd';
 // import { useRequest } from 'umi';
 // import request from 'umi-request';
 
 const index: React.FC = () => {
-
   return (
     <PageContainer>
       <p>Transaction data statistics(BSC)</p>
-      <Row gutter={16}>
+      <Row gutter={16} style={{ minWidth: 1240 }}>
         <Col span={4}>
           <Card>
             <Statistic groupSeparator="," title="NFTs on sale" value={256568} />

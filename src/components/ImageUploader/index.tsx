90a252d77c208dc55bdce5b0972c725bd098922a
IvanZen <247460386@qq.com>
2021-07-09 17:00:19 +0800

    🐞 fix(ImageUploader): add value

--- src/components/ImageUploader/index.tsx
+++ src/components/ImageUploader/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { message, Upload } from 'antd';
 // import ImgCrop from 'antd-img-crop';
 import { useState } from 'react';
-import { UploadFile, UploadChangeParam, RcFile } from 'antd/lib/upload/interface';
+import type { UploadFile, UploadChangeParam, RcFile } from 'antd/lib/upload/interface';
 import { PlusOutlined } from '@ant-design/icons';
 // import 'antd/dist/antd.min.css';
 import { fileUploader } from '@/services/uploader';
 
 export interface IImageUploaderProps {
-  value?: string;
+  value?: UploadFile;
   maxCount?: number;
   limit?: number;
   onChange: (file: UploadFile, items: UploadFile[]) => void;
@@ -21,7 +21,11 @@ const ImageUploader: React.FC<IImageUploaderProps> = ({
 }) => {
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   // const [loading, setLoading] = useState<boolean>(false);
-
+  useEffect(() => {
+    if (value) {
+      setFileList([value]);
+    }
+  }, [value]);
   const handleChange = ({ file, fileList: newFileList }: UploadChangeParam) => {
     setFileList(newFileList);
     if (file.status === 'done') {
@@ -30,15 +34,16 @@ const ImageUploader: React.FC<IImageUploaderProps> = ({
   };
 
   const handleBeforeUpload = async (file: RcFile) => {
-    if (limit && file.size > limit) {
+    const size = file.size || 0;
+    if (limit && size > limit) {
       message.error(`Image must smaller than ${limit}`);
       return false;
     }
-    console.log(2);
     // setLoading(true);
     const formData = new FormData();
     formData.append('filename', file);
     const res = await fileUploader(formData);
+    // eslint-disable-next-line no-param-reassign
     file.url = res.result?.path;
     // setLoading(false);
     return Promise.resolve();
@@ -46,16 +51,17 @@ const ImageUploader: React.FC<IImageUploaderProps> = ({
 
   return (
     // <ImgCrop rotate>
-      <Upload
-        maxCount={maxCount}
-        listType="picture-card"
-        accept="image/*"
-        showUploadList={{ showPreviewIcon: false }}
-        beforeUpload={handleBeforeUpload}
-        onChange={handleChange}
-      >
-        {maxCount > fileList.length ? <PlusOutlined /> : null}
-      </Upload>
+    <Upload
+      maxCount={maxCount}
+      listType="picture-card"
+      accept="image/*"
+      showUploadList={{ showPreviewIcon: false }}
+      beforeUpload={handleBeforeUpload}
+      onChange={handleChange}
+      fileList={fileList}
+    >
+      {maxCount > fileList.length ? <PlusOutlined /> : null}
+    </Upload>
     // </ImgCrop>
   );
 };
